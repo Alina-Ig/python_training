19231090c6cb2c8a4210c3a57b3cb407e413afbf
Alina <una.cherr@gmail.com>
2022-04-20 18:55:29 +0300

    Добавлен и отредактирован новый тестовый сценарий test_add_group: 1 удалены ненужные переменные, 2 удален метод close_alert_and_get_its_test 3 с помощью рефакторинга изменены названия переменных класса TestAddGroup и метода test_add_group 4 в кейсе добавлен комментарий с названием

--- test_add_group.py
+++ test_add_group.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+# Задание №1 для тренинга Software-Testing "Программирование на Python для тестировщиков"
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
